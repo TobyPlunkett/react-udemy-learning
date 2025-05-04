217bb7d4921ffc22369870917db6a21842d9e802
TobyPlunkett <tobyplunkett2@gmail.com>
2025-05-04 16:33:24 +0100

    #76 Add comment on isolated instances of components

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,6 +5,7 @@ return (
   <main>
     <div id="game-container">
       <ol id="players">
+        {/* these two Players are isolated instances. Clicking edit in one, will not change the state in the other */}
         <Player
           name="Player 1"
           symbol="X" />
