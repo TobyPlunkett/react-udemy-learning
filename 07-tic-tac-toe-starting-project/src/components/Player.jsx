d21149f6a20e8726f93dfcffe974a3c12f0a5c12
TobyPlunkett <tobyplunkett2@gmail.com>
2025-05-04 15:25:57 +0100

    #75 Start adding edit button

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false); 
+
+    function onClick(){
+        setIsEditing(!isEditing); 
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                
+                {
+                    isEditing ? (
+                        <span className="player-name">{name}</span>
+                    ) : (
+                        <input></input>
+                    )    
+                }
+                
+                
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>edit</button>
+            <button onClick={onClick}>edit</button>
         </li>
     );
 }
